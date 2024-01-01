9dd81922a7f0c93b97348ea6fdd75f1c09107d5d
Rangga056 <muhammadrangga056@gmail.com>
2024-01-01 07:35:06 +0700

    Complete header search function

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -2,9 +2,30 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { logo } from "../assets";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search/${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md font-poppins">
@@ -22,14 +43,21 @@ function Header() {
             </div>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             autoComplete="off"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-44 sm:w-80"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-start-500" />
+          <button>
+            <FaSearch className="text-start-500" />
+          </button>
         </form>
         <ul className="flex items-center sm:gap-4">
           <Link to="/">
