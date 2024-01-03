78e0fbda41523565f39dc28edd8140546088ff04
Rangga056 <muhammadrangga056@gmail.com>
2024-01-03 10:53:37 +0700

    Complete about page

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -2,8 +2,33 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <h1>About this page</h1>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Eclipse Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Eclipse Estate stands as the pinnacle of excellence in the realm of real
+        estate web applications, providing a meticulously curated platform
+        tailored for both seasoned investors and first-time homebuyers. Offering
+        an unparalleled blend of cutting-edge technology and unwavering
+        commitment to professionalism, our platform redefines the real estate
+        experience.
+      </p>
+      <p className="mb-4 text-slate-700">
+        For sellers, Eclipse Estate presents a sophisticated showcase for their
+        properties, enabling them to highlight every detail with precision.
+        Through high-quality visuals and comprehensive descriptions, sellers can
+        attract serious buyers, ensuring their listings are presented with the
+        utmost professionalism. On the buyer's side, our platform delivers a
+        seamless and refined exploration of a diverse range of properties,
+        supported by advanced search filters and real-time updates.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Eclipse Estate, we believe in transcending the transactional nature
+        of real estate. Our platform is a dynamic marketplace where
+        professionalism, transparency, and efficiency converge. Sellers showcase
+        their properties with confidence, and buyers explore listings with
+        assurance, all within an environment that prioritizes sophistication and
+        results.
+      </p>
     </div>
   );
 }
